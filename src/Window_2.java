aecc072367d7ef79e069141f29c1c0d069355d79
HengK2020 <65136595+HengK2020@users.noreply.github.com>
2021-02-11 14:18:44 -0600

    Incorporated Enc/Dec
    Updated window where it sends encrypt

--- src/Window_2.java
+++ src/Window_2.java
@@ -2,7 +2,21 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,13 +28,14 @@ public class Window_2 extends JFrame
 {
 	static String username2;
 	private JPanel contentPane;
-	
+	static Cipher cipher;
 	/**
 	 * Launch the application.
+	 * @throws NoSuchAlgorithmException 
 	 */
-	public static void main(String[] args) 
+	public static void main(String[] args)  
 	{
-		EventQueue.invokeLater(new Runnable() 
+	    EventQueue.invokeLater(new Runnable() 
 		{
 			public void run() 
 			{
@@ -42,7 +57,7 @@ public class Window_2 extends JFrame
 	public Window_2()
 	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 393, 499);
+		setBounds(100, 100, 747, 503);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -52,6 +67,11 @@ public class Window_2 extends JFrame
 		display2.setBounds(23, 48, 336, 344);
 		contentPane.setLayout(null);
 		contentPane.add(display2);
+		
+		display2_2 = new JTextArea();
+		display2_2.setBounds(370, 48, 336, 344);
+		contentPane.setLayout(null);
+		contentPane.add(display2_2);
 		
 		text2 = new JTextArea();
 		text2.setBounds(21, 403, 239, 45);
@@ -65,14 +85,34 @@ public class Window_2 extends JFrame
 				{
 					return;
 				}
-				display2.append(username2 + ":" + s + "\n");
-				Window_1.sendText();
+				//Displaying me instead of username2
+				display2.append("me" + " : " + s + "\n");
+				try {
+					Window_1.sendText();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} //save this line
 				text2.setText("");
 			}
 		});
 		send2.setBounds(270, 403, 89, 45);
 		contentPane.add(send2);
-		
+		////////////////Decrypt button///////////////////////
+		send2_1	= new JButton("DECRYPT");
+		send2_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0_) {
+				String s = text2.getText();
+				if(s.equals(" "));
+				{
+					return;
+				}
+			}
+		});
+		send2_1.setBounds(500, 403, 89, 45);
+		contentPane.add(send2_1);
+		////////////////////////////////////////////////////
+
 		label2 = new JLabel("Chat window for : " + username2);
 		label2.setBounds(21, 25, 250, 14);
 		contentPane.add(label2);
@@ -82,6 +122,7 @@ public class Window_2 extends JFrame
 			public void actionPerformed(ActionEvent e)
 			{
 				display2.setText("");
+				display2_2.setText("");
 			}
 		});
 		clear.setBounds(270, 21, 89, 23);
@@ -89,14 +130,49 @@ public class Window_2 extends JFrame
 		
 	}
 
-	public static void sendText()
+	public static void sendText() throws Exception
 	{
+		//////////////////////key gen ////////////////////////////////////
+		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+		keyGenerator.init(128); // block size is 128bits
+		SecretKey secretKey = keyGenerator.generateKey();
+		cipher = Cipher.getInstance("AES");
+		/////////////////////////////////////////////////////////////////////
+
 		String s = Window_1.text1.getText();
+		
+		//////////////////////for encryption////////////////////////////////
+		byte[] plainTextByte = s.getBytes();
+		try {
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		byte[] encryptedByte = null;
+		try {
+			encryptedByte = cipher.doFinal(plainTextByte);
+		} catch (IllegalBlockSizeException | BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Base64.Encoder encoder = Base64.getEncoder();
+		String encryptedText = encoder.encodeToString(encryptedByte);
+		///////////////////////////////////////////////////////////////////
+		
 		if(s.equals("")) 
 		{
 			return;
 		}
-		display2.append(Window_1.username1 + ":" + s + "\n");
+		//Saves what the is being typed in Window 2 and appends it to savedmsg.txt.
+		//File fileName = new File("savedmsg.txt");
+		//FileWriter fileWriter = new FileWriter(fileName, true);
+		//BufferedWriter buffer = new BufferedWriter(fileWriter);
+		//PrintWriter printWriter = new PrintWriter(buffer);
+		display2.append(Window_1.username1 + " : " + encryptedText + "\n");
+		display2_2.append(Window_2.username2 +  " : " + s + "\n");
+		//printWriter.println(Window_1.username1+ " : " + s);
+		//printWriter.close();
 	}
 	
 	
@@ -104,5 +180,8 @@ public class Window_2 extends JFrame
 	private static javax.swing.JTextArea display2;
 	private javax.swing.JButton send2;
 	public static javax.swing.JTextArea text2;
+	public static javax.swing.JTextArea display2_2;
+	private javax.swing.JButton send2_1;
+
 	
 }
